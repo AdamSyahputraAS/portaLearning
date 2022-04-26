d574e6caa102832c4902428b062d4ba26f1d4c03
satrio keren <satriotrinugroho@gmail.com>
2022-04-26 23:11:44 +0700

    Add "Add Content Activity Dan Layout"

--- app/src/main/java/com/adl/portalearning/Add_Content_Activity.kt
+++ app/src/main/java/com/adl/portalearning/Add_Content_Activity.kt
@@ -3,13 +3,9 @@ package com.adl.portalearning
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class login_activity : AppCompatActivity() {
+class Add_Content_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
-
+        setContentView(R.layout.activity_add_content)
     }
-
-
 }
