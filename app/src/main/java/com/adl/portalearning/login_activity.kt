404e1e7c53401dc115bf5680d945dbccae17dc1b
AdamSyahputraAS <adamsyahputra1797@gmail.com>
2022-05-07 02:18:34 +0700

    login admin dan user clear / adam

--- app/src/main/java/com/adl/portalearning/login_activity.kt
+++ app/src/main/java/com/adl/portalearning/login_activity.kt
@@ -3,6 +3,7 @@ package com.adl.portalearning
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -26,10 +27,20 @@ class login_activity : AppCompatActivity() {
         auth.signInWithEmailAndPassword(inputUsername.text.toString(),inputPasword.text.toString()).addOnCompleteListener{
                 it ->
             if (it.isSuccessful){
-                val intent = Intent(this,MainActivity::class.java)
-                startActivity(intent)
-                finish()
+                if(inputUsername.text.toString() == "admin@gmail.com "){
+                    val intent = Intent(this,Add_Content_Activity::class.java)
+                    startActivity(intent)
+                    finish()
+                }else {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
             }
+
+        }.addOnFailureListener{exception ->
+            Toast.makeText(applicationContext,"username, atau password anda salah",
+                Toast.LENGTH_LONG).show()
         }
     }
 
