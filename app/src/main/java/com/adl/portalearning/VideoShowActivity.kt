8daf6143d5c56598677c1c83a47ff43dc99518f4
madryanh <adryanh90@gmail.com>
2022-05-10 00:39:00 +0700

    videoView

--- app/src/main/java/com/adl/portalearning/VideoShowActivity.kt
+++ app/src/main/java/com/adl/portalearning/VideoShowActivity.kt
@@ -1,25 +1,59 @@
 package com.adl.portalearning
 
-import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
+import android.widget.MediaController
 import com.adl.portalearning.model.ModelVideo
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_add_content.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_video_show.*
 
 class VideoShowActivity : AppCompatActivity() {
 
     private lateinit var data: ModelVideo
+
+    private var videoUri: Uri? = null
+    private lateinit var videoArrayList: ArrayList<ModelVideo>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_show)
-        playVideo.setOnClickListener({
-            val intent = Intent(this@VideoShowActivity, webViewPlay::class.java)
-            intent.putExtra("Videos",data)
-            startActivity(intent)
+//        playVideo.setOnClickListener({
+//            val intent = Intent(this@VideoShowActivity, webViewPlay::class.java)
+//            intent.putExtra("Videos",data)
+//            startActivity(intent)
+//        })
+        setVideoToView()
+    }
+
+
+
+    fun setVideoToView() {
+        //set the picked video to video view
+
+        val videoUrl:String? = data.videoUri
+
+
+        //video play controls
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(playVideo)
+        val videoUri = Uri.parse(videoUrl)
+
+        //set media controller
+        playVideo.setMediaController(mediaController)
+        //set video uri
+        playVideo.setVideoURI(videoUri)
+        playVideo.requestFocus()
+        playVideo.setOnPreparedListener({
+            //when video is ready, by default dont play automaticly
+            playVideo.start()
         })
     }
+
 }
--- app/src/main/java/com/adl/portalearning/adapter/VideoViewHolder.kt
+++ app/src/main/java/com/adl/portalearning/adapter/VideoViewHolder.kt
@@ -2,14 +2,10 @@ package com.adl.portalearning.adapter
 
 import android.content.Intent
 import android.view.View
-import androidx.cardview.widget.CardView
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.adl.portalearning.Add_Content_Activity
 import com.adl.portalearning.VideoShowActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import kotlinx.android.synthetic.main.item_main_activity.*
 import kotlinx.android.synthetic.main.item_main_activity.view.*
 
 class VideoViewHolder(view: View): RecyclerView.ViewHolder(view) {
